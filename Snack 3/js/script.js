b9718000dec7e58865de63124c55f655e0ea2294
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-05-11 17:13:56 +0200

    fine

--- Snack 3/js/script.js
+++ Snack 3/js/script.js
@@ -1,2 +1,16 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). 
 // La funzione restituirà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+function numericompresi (arr, min, max) {
+    
+    for (var i = min; i < max - 1; i++) {
+        arr.push(i + 1);
+    }
+    
+    return arr;
+}
+
+var numeri = [];
+numericompresi (numeri, 1, 50);
+
+console.log(numeri);
