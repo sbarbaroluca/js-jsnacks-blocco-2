edc8f85bc7f9badaf08e5654efef14b198b6589d
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-05-11 14:58:27 +0200

    inizio

---
+++ Snack 3/js/script.js
@@ -0,0 +1,2 @@
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). 
+// La funzione restituirà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
